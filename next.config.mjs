ee36c747f1bf9e871522d78b68dae8ea9e55a832
Belurgas <70036882+belurgas@users.noreply.github.com>
2024-12-11 21:32:00 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    headers: [
+        {
+          key: 'X-Accel-Buffering',
+          value: 'no',
+       },
+    ],
     images: {
         dangerouslyAllowSVG: true,
         remotePatterns: [
